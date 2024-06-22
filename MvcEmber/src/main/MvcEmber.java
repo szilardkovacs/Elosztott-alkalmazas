1027e6796f584242e6aaaa226064d6ff6a03ae92
szilardkovacs <64313996+szilardkovacs@users.noreply.github.com>
2024-06-22 10:09:53 +0200

    Add files via upload
    GuiNezet hozzaadasa

--- MvcEmber/src/main/MvcEmber.java
+++ MvcEmber/src/main/MvcEmber.java
@@ -1,6 +1,10 @@
 package main;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import modell.EmberModell;
+import nezet.GuiNezet;
 import nezet.KonzolNezet;
 import nezet.Nezet;
 
@@ -16,8 +20,27 @@ public class MvcEmber {
         System.out.println(modell.toString());
         System.out.println(modell.bemutatkozas());
         
-        Nezet nezet = new KonzolNezet();
-        nezet.megjelenito("Konzolteszt nev: " + nezet.getNev() + ", kor: " + nezet.getKor(nezet.getSzulev()) + ", magassag: " + nezet.getMagassag());
+        //Nezet nezet = new KonzolNezet();
+        //nezet.megjelenito("Konzolteszt nev: " + nezet.getNev() + ", kor: " + nezet.getKor(nezet.getSzulev()) + ", magassag: " + nezet.getMagassag());
+        
+        GuiNezet nezet = new GuiNezet();
+        nezet.setVisible(true);
+        
+        JButton btn = ((GuiNezet)nezet).getBtnMegjelenit();
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nev = nezet.getNev();
+                int szulev = nezet.getSzulev();
+                int magassag = nezet.getMagassag();
+                modell.setNev(nev);
+                modell.setSzulev(szulev);
+                modell.setMagassag(magassag);
+                String info = "Bemenet: " + modell.toString() + "\n" ;
+                info += "Guiadatok: " + modell.bemutatkozas();
+                nezet.megjelenito(info);
+            }
+        });
     }
     
 }
